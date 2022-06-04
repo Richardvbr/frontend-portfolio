ff29eb6d61902ba45cac51aecd0405e12d28f4ed
Richard van Brunschot <richardvanbrunschot@gmail.com>
2022-06-04 20:21:38 +0200

    post progress scroll

--- src/components/blog/post/index.jsx
+++ src/components/blog/post/index.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import ProgressBar from 'react-scroll-progress-bar';
 import { MDXRemote } from 'next-mdx-remote';
 import { HiChevronDoubleLeft } from 'react-icons/hi';
 
@@ -7,15 +8,18 @@ import Link from 'next/link';
 
 const Post = ({ post }) => {
   return (
-    <S.Container>
-      <Link href={'/blog'} passHref>
-        <a className='back-link'>
-          <HiChevronDoubleLeft />
-          Back to blog posts
-        </a>
-      </Link>
-      <MDXRemote {...post.source} components={{ Image }} />
-    </S.Container>
+    <>
+      <ProgressBar bgcolor='#119d73' />
+      <S.Container>
+        <Link href={'/blog'} passHref>
+          <a className='back-link'>
+            <HiChevronDoubleLeft />
+            Back to blog posts
+          </a>
+        </Link>
+        <MDXRemote {...post.source} components={{ Image }} />
+      </S.Container>
+    </>
   );
 };
 
